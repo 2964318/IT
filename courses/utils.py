a0fdc6844fd20ce2dd23f85ee715b78c34420130
yangjl1015 <128110712+yangjl1015@users.noreply.github.com>
2025-03-13 17:05:56 +0000

    Update utils.py

--- courses/utils.py
+++ courses/utils.py
@@ -1,14 +1,15 @@
 from .models import CustomUser, Notification
 
-def notify_user(user, message, is_admin=False, is_global=False):
+def notify_user(user,title, message, is_admin=False, is_global=False):
     Notification.objects.create(
         user=user,
         message=message,
         is_admin=is_admin,
-        is_global=is_global
+        is_global=is_global,
+        title=title
     )
 
 def notify_admins(message):
     admins = CustomUser.objects.filter(is_staff=True)
     for admin in admins:
-        notify_user(admin, message, is_admin=True)
+        notify_user(admin, message,  is_admin=True)
